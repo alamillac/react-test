d599b66e80b325206007d83e89f95d5017f3ca43
Andres <andres@lamillac.com>
2017-07-16 09:49:53 +0200

    Init commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+/*
+    Entry file
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  title: 'React App Setup',
+  filename: 'index.html',
+  template: './src/index.ejs',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
