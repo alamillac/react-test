a4eca24b91e75278818db94930e06d7e6cbed881
Andres <andres@lamillac.com>
2017-07-16 10:39:58 +0200

    Minnor changes in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-/*
-    ./webpack.config.js
-*/
 const path = require('path');
 
+const APP_SRC = path.resolve(__dirname, 'src');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  title: 'React App Setup',
-  filename: 'index.html',
-  template: './src/index.ejs',
-  inject: 'body'
-});
 
 module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    path: path.resolve('dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
-  },
-  plugins: [HtmlWebpackPluginConfig]
+    entry: APP_SRC + '/js/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'React App Setup',
+            filename: 'index.html',
+            template: APP_SRC + '/index.ejs',
+            inject: 'body'
+        })
+    ],
+    resolve: {
+        modules: [path.resolve(APP_SRC, 'js'), "node_modules"]
+    }
 }
